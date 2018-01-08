4b822ad2e08adc83a3f701aecdf097a115a7b571
wanching0730 <wanching_97@hotmail.com>
2018-01-08 16:28:59 +0800

    done

---
+++ Quizzler/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Quizzler
+//
+//  Created by Wan Ching on 04/01/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +24,83 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+        nextQuestion()
   
     }
     
-    
+    // update all UI view (progress bar, score label)
     func updateUI() {
+        
+        scoreLabel.text = "\(score)"
+        progressLabel.text = "\(questionNumber + 1)/13"
+        
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
       
     }
     
-
-    func nextQuestion() {
-        
-    }
-    
-    
+    // check whether the user got the right answer
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            ProgressHUD.showSuccess("Correct!")
+            score += 1
+        }
+        else {
+            ProgressHUD.showError("Wrong!")
+        }
+        
+    }
+    
+    // update question text and check whether we come to the end
+    func nextQuestion() {
+        
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
+        }
+        else {
+            
+            let alert = UIAlertController(title: "Awesome", message: "The quiz is ended, do you want to start all over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in
+                self.startOver()
+            })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
     }
     
     
+    // wipe the board clean, so user can retake the quiz
     func startOver() {
        
+        questionNumber = 0
+        nextQuestion()
+        
     }
     
 
